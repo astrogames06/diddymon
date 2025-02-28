d4edf9a6758bf93ec3fa443bdb52696fa94db230
Astro <88717385+MilkMouseBearCoder@users.noreply.github.com>
2025-02-28 14:48:04 +1100

    0m

--- wbuild.sh
+++ wbuild.sh
@@ -9,6 +9,7 @@ EMFLAGS="-s USE_GLFW=3 --shell-file build/web/shell.html -DPLATFORM_WEB"
 INCLUDE_DIR="include/"
 LIB_DIR="lib/"
 OUTPUT="build/web/index.html"
+FILES="src/main.cpp src/Game/Game.cpp src/Entity/Entity.cpp src/Player/Player.cpp"
 
 # Build command
-$CC -o $OUTPUT src/main.cpp $LIB_DIR/libraylib.web.a $CFLAGS -I. -I $INCLUDE_DIR -L. -L $LIB_DIR $EMFLAGS
+$CC -o $OUTPUT $FILES $LIB_DIR/libraylib.web.a $CFLAGS -I. -I $INCLUDE_DIR -L. -L $LIB_DIR $EMFLAGS
